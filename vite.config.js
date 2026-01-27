1202bc5dc57ce46e1095b80aa99e6a7bf300ccda
malrjane <yana.ml02@mail.ru>
2026-01-27 19:12:51 +0700

    deploy

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   assetsInclude: ['**/*.avif', '**/*.webp'],
+   base: '/bird-casino/',
   
   // Добавьте явную обработку avif
   build: {
@@ -16,3 +17,4 @@ export default defineConfig({
     include: ['**/*.avif']
   }
 });
+ 
